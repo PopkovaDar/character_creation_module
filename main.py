b613d61459fb647bde7c3e36f042e7ca98f0e746
Попкова Дарья <daryapopkova90@yandex.ru>
2023-06-06 15:28:50 +0300

    code according too  PEP8

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
+"""В этом коде ниже функции для выбора игрока, его умений и тд."""
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Эта функция считает нанесение урона."""
     if char_class == 'warrior':
         print(f'{char_name} нанёс урон противнику равный {5+randint(3, 5)}')
     if char_class == 'mage':
@@ -12,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Эта функция считает сколько заблокировано урона."""
     if char_class == 'warrior':
         print(f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -21,6 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Эта функция говорит какое было применено умение."""
     if char_class == 'warrior':
         print(f'{char_name} применил специальное '
               f'умение «Выносливость {80 + 25}»')
@@ -31,6 +38,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Эта функция рассказывает какие навыки у выбранного героя."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -56,6 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """В этой функции происходит выбор персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -77,7 +86,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+def main() -> None:
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -87,3 +97,6 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+    """Эта функция приветствие."""
+
+    main()
